4a786fc16a7bab0b93f963057322acb65fef7099
Bakhodirbekov <otabekotabek1661@gmail.com>
2023-05-01 05:50:34 -0700

    new pages menu3

--- src/main/java/com/company/Avto/Conntrolers/Bolgcontroller.java
+++ src/main/java/com/company/Avto/Conntrolers/Bolgcontroller.java
@@ -1,5 +1,6 @@
 package com.company.Avto.Conntrolers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,14 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Bolgcontroller {
 
-   @GetMapping("/index2")
-   public String blogMain(Model model) {
-       model.addAttribute("title","index1");
-       return "blog-main";
-   }
-   @GetMapping("/index2")
-   public String BlogMain( Model model) {
-       model.addAttribute("title","index1");
-       return "blog-main";
-   }
+    @GetMapping("/index2")
+    public String blogMain(Model model) {
+        model.addAttribute("title", "index2");
+        return "blog-main";
+    }
+
+    @GetMapping("/index")
+    public String BlogMain(Model model) {
+        model.addAttribute("title", "index2");
+        return "blog-main";
+    }
 }
